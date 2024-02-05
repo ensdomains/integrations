a575b1f43b595112d791951d1d2cd25f1c2e01da
v1rtl <hi@v1rtl.site>
2024-02-05 15:09:48 +0200

    fix the script to include other link data

--- index.ts
+++ index.ts
@@ -1,27 +1,36 @@
-
 type Links<T = unknown> = Record<
-string,
-{ link: string; name: string } & T
+  string,
+  & {
+    link: string
+    name: string
+    featuredApp?: number
+    featuredBrowser?: number
+  }
+  & T
 >
-
 
 const links = JSON.parse(await Deno.readTextFile('./links.json')) as Links
 
 const newLinks: Links = {}
-const deadLinks: Links<{cause: unknown}> = {}
+const deadLinks: Links<{ cause: unknown }> = {}
 
-for (const [key, { name, link }] of Object.entries(links)) {
+for (const [key, { link, ...rest }] of Object.entries(links)) {
   try {
-    const res = await fetch(link, { signal: AbortSignal.timeout(10_000),cache:'no-store' })
+    const res = await fetch(link, {
+      signal: AbortSignal.timeout(10_000),
+      cache: 'no-store',
+    })
     console.log(`${key}: status ${res.status}`)
 
-    newLinks[key] = { name, link }
+    newLinks[key] = { link, ...rest }
   } catch (err) {
     console.log(key, err)
-    deadLinks[key] = { name, link, cause: (err as Error).message }
-  } 
+    deadLinks[key] = { link, cause: (err as Error).message, ...rest }
+  }
 }
 
-
-await Deno.writeTextFile('./dead-links.json', JSON.stringify(deadLinks, null, 2))
+await Deno.writeTextFile(
+  './dead-links.json',
+  JSON.stringify(deadLinks, null, 2),
+)
 await Deno.writeTextFile('./links.json', JSON.stringify(newLinks, null, 2))
